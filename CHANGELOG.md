4ab2db6630f7a5b3785bbb92bba346bc20c74a0d
jg-development <jan.gantzert@template-provider.com>
2020-08-04 12:42:29 +0200

    fix price display while logged in

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.2
+### Fixed
+- Fix showing login text, after already logged in
--- Pricing/Render/FinalPriceBox.php
+++ Pricing/Render/FinalPriceBox.php
@@ -2,12 +2,48 @@
 
 namespace TemplateProvider\Hideprice\Pricing\Render;
 
+use Magento\Catalog\Model\Product\Pricing\Renderer\SalableResolverInterface;
+use Magento\Customer\Model\Session;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Pricing\SaleableInterface;
+use Magento\Framework\Pricing\Price\PriceInterface;
+use Magento\Framework\Pricing\Render\RendererPool;
+use Magento\Catalog\Pricing\Price\MinimalPriceCalculatorInterface;
+
 class FinalPriceBox extends \Magento\Catalog\Pricing\Render\FinalPriceBox
 {
+    /** @var \Magento\Framework\App\Http\Context */
+    protected $httpContext;
+
+    /**
+     * @param Context $context
+     * @param SaleableInterface $saleableItem
+     * @param PriceInterface $price
+     * @param RendererPool $rendererPool
+     * @param array $data
+     * @param SalableResolverInterface $salableResolver
+     * @param MinimalPriceCalculatorInterface $minimalPriceCalculator
+     * @param \Magento\Framework\App\Helper\Context $httpContext
+     */
+    public function __construct(
+        Context $context,
+        SaleableInterface $saleableItem,
+        PriceInterface $price,
+        RendererPool $rendererPool,
+        array $data = [],
+        \Magento\Framework\App\Http\Context $httpContext,
+        SalableResolverInterface $salableResolver = null,
+        MinimalPriceCalculatorInterface $minimalPriceCalculator = null
+    ) {
+        $this->httpContext = $httpContext;
+        parent::__construct($context, $saleableItem, $price, $rendererPool, $data, $salableResolver, $minimalPriceCalculator);
+    }
+
     protected function _toHtml()
     {
+        $isLoggedIn = $this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
         $value = $this->_scopeConfig->getValue('catalog/available/hide_price_text', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        if (!empty($value)) {
+        if (!empty($value) && !$isLoggedIn) {
             return $value;
         }
         return parent::_toHtml();
